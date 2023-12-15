23f82c04284e1d9e075110a09d94c63b828421ff
javamogi <40781237+javamogi@users.noreply.github.com>
2023-12-15 13:13:04 +0900

    test: 로그인 성공 test 추가

--- src/test/java/com/phcworld/user/service/UserServiceTest.java
+++ src/test/java/com/phcworld/user/service/UserServiceTest.java
@@ -2,6 +2,8 @@ package com.phcworld.user.service;
 
 import com.phcworld.exception.model.DuplicationException;
 import com.phcworld.exception.model.NotFoundException;
+import com.phcworld.jwt.TokenProvider;
+import com.phcworld.jwt.dto.TokenDto;
 import com.phcworld.user.domain.Authority;
 import com.phcworld.user.domain.User;
 import com.phcworld.user.dto.LoginUserRequestDto;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.BadCredentialsException;
 
@@ -81,6 +84,22 @@ class UserServiceTest {
         Assertions.assertThrows(BadCredentialsException.class, () -> {
             userService.tokenLogin(requestDto);
         });
+    }
+
+    @Test
+    void 로그인_성공(){
+        LoginUserRequestDto requestDto = LoginUserRequestDto.builder()
+                .email("test@test.test")
+                .password("test")
+                .build();
+        TokenDto tokenDto = TokenDto.builder()
+                .grantType("grantType")
+                .accessToken("accessToken")
+                .refreshToken("refreshToken")
+                .build();
+        when(userService.tokenLogin(requestDto)).thenReturn(tokenDto);
+        TokenDto resultToken = userService.tokenLogin(requestDto);
+        assertThat(resultToken).isEqualTo(tokenDto);
     }
 
 }
