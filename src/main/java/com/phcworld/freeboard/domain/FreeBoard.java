207f271edbbb5b1a7825b513e14d9d0dad038ee6
javamogi <40781237+javamogi@users.noreply.github.com>
2023-12-22 11:14:12 +0900

    feat: 게시판 CRUD 추가

---
+++ src/main/java/com/phcworld/freeboard/domain/FreeBoard.java
@@ -0,0 +1,106 @@
+package com.phcworld.freeboard.domain;
+
+import com.phcworld.freeboard.dto.FreeBoardRequestDto;
+import com.phcworld.user.domain.User;
+import com.phcworld.utils.LocalDateTimeUtils;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Accessors(chain = true)
+@EntityListeners(AuditingEntityListener.class)
+@DynamicUpdate
+@Table(name = "free_board",
+		indexes = {@Index(name = "idx__create_date", columnList = "createDate"),
+				@Index(name = "idx__writer_id_create_date", columnList = "writer_id, createDate")})
+//@SequenceGenerator(
+//		name = "BOARD_SEQ_GENERATOR",
+//		sequenceName = "BOARD_SEQ",
+//		initialValue = 1, allocationSize = 10000
+//)
+public class FreeBoard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_freeBoard_writer"))
+	private User writer;
+
+	private String title;
+
+	@Lob
+	private String contents;
+
+	@CreatedDate
+	private LocalDateTime createDate;
+	
+	@LastModifiedDate
+	private LocalDateTime updateDate;
+
+	@ColumnDefault("0")
+	private Integer count;
+
+	private Boolean isDeleted;
+
+//	@OneToMany(mappedBy = "freeBoard", cascade = CascadeType.REMOVE)
+	// @JsonManagedReference
+//	@JsonBackReference
+//	private List<FreeBoardAnswer> freeBoardAnswers;
+
+//	public String getCountOfAnswer() {
+//		if (this.freeBoardAnswers == null|| this.freeBoardAnswers.size() == 0) {
+//			return "";
+//		}
+//		return "[" + freeBoardAnswers.size() + "]";
+//	}
+
+	public void addCount() {
+		this.count += 1;
+	}
+
+	public String getFormattedCreateDate() {
+		return LocalDateTimeUtils.getTime(createDate);
+	}
+	
+	public String getFormattedUpdateDate() {
+		return LocalDateTimeUtils.getTime(updateDate);
+	}
+
+	public void update(FreeBoardRequestDto request) {
+		this.title = request.title();
+		this.contents = request.contents();
+	}
+
+	public Boolean isNew(){
+		final int HOUR_OF_DAY = 24;
+		final int MINUTES_OF_HOUR = 60;
+
+		long createdDateAndNowDifferenceMinutes =
+				Duration.between(createDate == null ? LocalDateTime.now() : createDate, LocalDateTime.now()).toMinutes();
+        return (createdDateAndNowDifferenceMinutes / MINUTES_OF_HOUR) < HOUR_OF_DAY;
+	}
+
+	public boolean matchUser(Long userId) {
+		return !this.writer.getId().equals(userId);
+	}
+
+	public void delete() {
+		this.isDeleted = true;
+	}
+}
---
+++ src/main/java/com/phcworld/freeboard/service/FreeBoardService.java
@@ -0,0 +1,101 @@
+package com.phcworld.freeboard.service;
+
+import com.phcworld.common.dto.SuccessResponseDto;
+import com.phcworld.exception.model.NotFoundException;
+import com.phcworld.exception.model.UnauthorizedException;
+import com.phcworld.freeboard.domain.FreeBoard;
+import com.phcworld.freeboard.dto.*;
+import com.phcworld.freeboard.repository.FreeBoardRepository;
+import com.phcworld.security.utils.SecurityUtil;
+import com.phcworld.user.domain.Authority;
+import com.phcworld.user.domain.User;
+import com.phcworld.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FreeBoardService {
+	private final FreeBoardRepository freeBoardRepository;
+	private final UserRepository userRepository;
+
+//	private final TimelineServiceImpl timelineService;
+	
+//	private final AlertServiceImpl alertService;
+
+	@Transactional
+	public FreeBoardResponseDto registerFreeBoard(FreeBoardRequestDto request) {
+		Long userId = SecurityUtil.getCurrentMemberId();
+		User user = userRepository.findById(userId)
+				.orElseThrow(NotFoundException::new);
+		FreeBoard freeBoard = FreeBoard.builder()
+				.writer(user)
+				.title(request.title())
+				.contents(request.contents())
+				.build();
+
+//		timelineService.createTimeline(createdFreeBoard);
+		return FreeBoardResponseDto.of(freeBoardRepository.save(freeBoard));
+	}
+
+	@Transactional(readOnly = true)
+	public List<FreeBoardResponseDto> getSearchList(FreeBoardSearchDto search) {
+		PageRequest pageRequest = PageRequest.of(search.pageNum() - 1, search.pageSize(), Sort.by("createDate").descending());
+		List<FreeBoardSelectDto> list = freeBoardRepository.findByKeyword(search, pageRequest);
+		return list.stream()
+				.map(FreeBoardResponseDto::of)
+				.toList();
+	}
+
+	@Transactional
+	public FreeBoardResponseDto getFreeBoard(Long id) {
+		FreeBoard freeBoard = freeBoardRepository.findById(id)
+				.orElseThrow(NotFoundException::new);
+		freeBoard.addCount();
+		return FreeBoardResponseDto.of(freeBoard);
+	}
+
+	@Transactional
+	public FreeBoardResponseDto updateFreeBoard(FreeBoardRequestDto request) {
+		FreeBoard freeBoard = freeBoardRepository.findById(request.id())
+				.orElseThrow(NotFoundException::new);
+
+		Long userId = SecurityUtil.getCurrentMemberId();
+		Authority authorities = SecurityUtil.getAuthorities();
+
+		if(freeBoard.matchUser(userId) && authorities != Authority.ROLE_ADMIN){
+			throw new UnauthorizedException();
+		}
+
+		freeBoard.update(request);
+		return FreeBoardResponseDto.of(freeBoard);
+	}
+
+	@Transactional
+	public SuccessResponseDto deleteFreeBoard(Long id) {
+		FreeBoard freeBoard = freeBoardRepository.findById(id)
+				.orElseThrow(NotFoundException::new);
+
+		Long userId = SecurityUtil.getCurrentMemberId();
+		Authority authorities = SecurityUtil.getAuthorities();
+
+		if(freeBoard.matchUser(userId) && authorities != Authority.ROLE_ADMIN){
+			throw new UnauthorizedException();
+		}
+
+//		timelineService.deleteTimeline(SaveType.FREE_BOARD, id);
+//		alertService.deleteAlert(SaveType.FREE_BOARD, id);
+		freeBoard.delete();
+
+		return SuccessResponseDto.builder()
+				.statusCode(200)
+				.message("삭제 성공")
+				.build();
+	}
+
+}
